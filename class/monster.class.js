f3044e3edf4a702b0de02c7ea9bb23cc2e41ed62
Jan Sobeslav <sobeslav.jan@seznam.cz>
2021-01-09 01:37:31 +0100

    monster class weak, name input

--- class/monster.class.js
+++ class/monster.class.js
@@ -17,9 +17,31 @@ class Monster {
         return Math.floor(Math.random() * (this.maxDam - this.minDam + 1) ) + this.minDam;
     }
     obnoveni() {
+        let cislo = Math.floor(Math.random() * 6);
+        switch(cislo) {
+            case 0:
+                this.weak = 'sword';
+                break;
+            case 1:
+                this.weak = 'axe';
+                break;
+            case 2:
+                this.weak = 'flail';
+                break;
+            case 3:
+                this.weak = 'bow';
+                break;
+            case 4:
+                this.weak = 'fspell';
+                break;
+            case 5:
+                this.weak = 'wspell';
+                break;
+            }
         this.level++;
         this.health = 100*((this.level+1)/2);
         this.minDam += 2*this.level;
         this.maxDam += 2.5*this.level;
+
     }
 }
