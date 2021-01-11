fb5ab368e831a161d7b3004865d2a7198923065a
Jan Sobeslav <sobeslav.jan@seznam.cz>
2021-01-11 12:08:23 +0100

    basic oracle system

--- class/oracle.class.js
+++ class/oracle.class.js
@@ -4,9 +4,34 @@ class Oracle {
     }
     vypsatVyhodu(benefit){
         let kostka = Math.floor(Math.random() * 11);
-        if (kostka === 0 || 5) {
-            return "Smůla se ti lepí na paty!";
+        if (kostka >= 0 && kostka <= 5) {
+            return "Smůla se ti lepí na paty! Zaplať 50 zlatých a zkus to znovu!";
         } else {
+            switch (benefit) {
+                case 'sword':
+                    benefit = "meč";
+                    break;
+  
+                case 'axe':
+                    benefit = "sekeru";
+                    break;
+
+                case 'flail':
+                    benefit = "kyj";
+                    break;
+
+                case 'bow':
+                    benefit = "luk";
+                    break;
+
+                case 'fspell':
+                    benefit = "ohnivé kouzlo";
+                    break;
+
+                case 'wspell':
+                    benefit = "ledové nebo vodní kouzlo";
+                    break;
+            }
             return "Proti tvému příštímu nepříteli použij "+benefit+"!";
         }
     }
